fb1fecb124200d528cbe15176957752adc92440a
sm8139-rgb <sm8139@srmist.edu.in>
2026-02-24 10:43:56 +0530

    UC5

---
+++ UC5OOPS.java
@@ -0,0 +1,19 @@
+public class UC5OOPS{
+	public static void main(String[] args){
+		String[] lines = {
+		  String.join(" ","    ***   " , "    ***   " , "***** " , "  ***** "),
+		  String.join(" "," **    ** " , " **    ** " , "*   **" , " **     "),
+		  String.join(" ","**      **" , "**      **" , "*   **" , "**      "),
+		  String.join(" ","**      **" , "**      **" , "*  ** " , " *****  "),
+		  String.join(" ","**      **" , "**      **" , "***   " , "     ** "),
+		  String.join(" "," **    ** " , " **    ** " , "*     " , " **  ** "),
+		  String.join(" ","   ***    " , "   ***    " , "*     " , "  ***** ")};
+		 
+		for(int i=0;i<7;i++){
+			System.out.println(lines[i]);
+		}
+	
+		}
+	
+	}
+
